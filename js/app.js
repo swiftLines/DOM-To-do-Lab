b8db6bff74799660e62b3a8faa47b465aec5d78d
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-03 07:30:27 -0500

    create chached references & add event Listener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('sanity check') 
+//Cached Element References
+//const inpElement = document.getElementById('id')
+const btnElement = document.getElementById('submit-button')
+const ulElement = document.getElementById("todo-list")
+
+btnElement.addEventListener('click', function(evt) {
+    const newLi = document.createElement('li')
+
+    const inpElement = document.querySelector('id')
+
+    newLi.textContent = inpElement.value
+
+    document.querySelector('ul').appendChild(newLi)
+
+    inpElement.value = ""
+
+    
+})
