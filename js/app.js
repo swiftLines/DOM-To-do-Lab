9208af4f08f36ce8c0025b93ca6e257b04bc9552
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-04 08:13:03 -0500

    Delete item on click

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const btnElement = document.getElementById('submit-button')
 const ulElement = document.getElementById("todo-list")
 const span = document.querySelector('span')
 
+
 btnElement.addEventListener('click', function(evt) {
     const newLi = document.createElement('li')
 
@@ -16,12 +17,23 @@ btnElement.addEventListener('click', function(evt) {
     }
 })
 
+//create reset button
 const resetBtn = document.createElement('button')
 resetBtn.innerHTML = "Reset"
 //append
 span.appendChild(resetBtn)
 
+//make reset button functional
 resetBtn.addEventListener('click', function(evt) {
     //clear ulElement
     ulElement.innerHTML = ''
 })
+
+//Delete an Item on Click
+
+ulElement.addEventListener('click', function(evt) {
+    let liElement = document.querySelector('li')
+    liElement.remove(evt.target)
+})
+
+
